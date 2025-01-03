d8365e256db5ef0627c9c43f1c36cb6a142c67dc
umitsahin <umitsahin21@gmail.com>
2025-01-03 12:50:49 +0300

    responsive deneme

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     toast.success("Sayfama Hoşgeldiniz!");
   }, []);
   return (
-    <div className="flex flex-col items-center gap-20 ">
+    <div className="flex flex-col items-center gap-20 px-2 lg:px-0 w-full min-h-screen">
       <Welcome />
       <Skills />
       <Profile />
